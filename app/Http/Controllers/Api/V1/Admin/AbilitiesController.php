b1b5af5aa5586c62fc5571d801a7d1457be5ab52
GZSistemasSF <gzsistemassf@gmail.com>
2022-06-08 18:00:55 -0300

    bypass roles

--- app/Http/Controllers/Api/V1/Admin/AbilitiesController.php
+++ app/Http/Controllers/Api/V1/Admin/AbilitiesController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\AbilityResource;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class AbilitiesController extends Controller
 {
     public function index()
     {
-        $permissions = auth()->user()->roles()->with('permissions')->get()
+        $permissions = /* auth()->user()->roles()-> */Role::with('permissions')->get()
             ->pluck('permissions')
             ->flatten()
             ->pluck('title')
