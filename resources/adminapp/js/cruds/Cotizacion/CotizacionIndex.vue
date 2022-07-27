6176c44d08565c8f292062b42d24f319d8ab91b6
GonzaloBernard <gonza.bernard.sf@gmail.com>
2022-07-27 17:58:03 -0300

    Quitado del boton actualizar en indices

--- resources/adminapp/js/cruds/Cotizacion/CotizacionIndex.vue
+++ resources/adminapp/js/cruds/Cotizacion/CotizacionIndex.vue
@@ -1,29 +1,32 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card">
-          <div class="card-header card-header-primary card-header-icon">
-            <div class="card-icon">
-              <i class="material-icons">assignment</i>
-            </div>
-            <h4 class="card-title">
-              {{ $t('global.table') }}
-              <strong>Cotizaciones</strong>
-            </h4>
-          </div>
-          <div class="card-body">
-            <router-link
-              class="btn btn-primary"
-              v-if="$can(xprops.permission_prefix + 'create')"
-              :to="{ name: xprops.route + '.create' }"
-            >
-              <!-- <i class="material-icons">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div
+                        class="card-header card-header-primary card-header-icon"
+                    >
+                        <div class="card-icon">
+                            <i class="material-icons">assignment</i>
+                        </div>
+                        <h4 class="card-title">
+                            {{ $t("global.table") }}
+                            <strong>Cotizaciones</strong>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <router-link
+                            class="btn btn-primary"
+                            v-if="$can(xprops.permission_prefix + 'create')"
+                            :to="{ name: xprops.route + '.create' }"
+                        >
+                            <!-- <i class="material-icons">
                 add
               </i> -->
-              Ver Carro de Cotización
-            </router-link>
-            <button
+                            Ver Carro de Cotización
+                        </router-link>
+                        <!--
+<button
               type="button"
               class="btn btn-default"
               @click="fetchIndexData"
@@ -35,112 +38,123 @@
               </i>
               {{ $t('global.refresh') }}
             </button>
-          </div>
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="table-overlay" v-show="loading">
-                  <div class="table-overlay-container">
-                    <material-spinner></material-spinner>
-                    <span>Loading...</span>
-                  </div>
+-->
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="table-overlay" v-show="loading">
+                                    <div class="table-overlay-container">
+                                        <material-spinner></material-spinner>
+                                        <span>Loading...</span>
+                                    </div>
+                                </div>
+                                <datatable
+                                    :columns="columns"
+                                    :data="data"
+                                    :total="total"
+                                    :query="query"
+                                    :xprops="xprops"
+                                    :HeaderSettings="false"
+                                    :pageSizeOptions="[10, 25, 50, 100]"
+                                >
+                                    <global-search
+                                        :query="query"
+                                        class="pull-left"
+                                    />
+                                    <header-settings
+                                        :columns="columns"
+                                        class="pull-right"
+                                    />
+                                </datatable>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <datatable
-                  :columns="columns"
-                  :data="data"
-                  :total="total"
-                  :query="query"
-                  :xprops="xprops"
-                  :HeaderSettings="false"
-                  :pageSizeOptions="[10, 25, 50, 100]"
-                >
-                  <global-search :query="query" class="pull-left" />
-                  <header-settings :columns="columns" class="pull-right" />
-                </datatable>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import DatatableActions from '@components/Datatables/DatatableActions'
-import TranslatedHeader from '@components/Datatables/TranslatedHeader'
-import HeaderSettings from '@components/Datatables/HeaderSettings'
-import GlobalSearch from '@components/Datatables/GlobalSearch'
-import Clientes from '@components/Cotizaciones/Clientes'
-import Productos from '@components/Cotizaciones/Productos'
+import { mapGetters, mapActions } from "vuex";
+import DatatableActions from "@components/Datatables/DatatableActions";
+import TranslatedHeader from "@components/Datatables/TranslatedHeader";
+import HeaderSettings from "@components/Datatables/HeaderSettings";
+import GlobalSearch from "@components/Datatables/GlobalSearch";
+import Clientes from "@components/Cotizaciones/Clientes";
+import Productos from "@components/Cotizaciones/Productos";
 
 export default {
-  components: {
-    GlobalSearch,
-    HeaderSettings
-  },
-  data() {
-    return {
-      columns: [
-        {
-          title: 'cruds.income.fields.id',
-          field: 'id',
-          thComp: TranslatedHeader,
-          sortable: true,
-          colStyle: 'width: 100px;'
+    components: {
+        GlobalSearch,
+        HeaderSettings,
+    },
+    data() {
+        return {
+            columns: [
+                {
+                    title: "cruds.income.fields.id",
+                    field: "id",
+                    thComp: TranslatedHeader,
+                    sortable: true,
+                    colStyle: "width: 100px;",
+                },
+                {
+                    title: "Clientes",
+                    thComp: TranslatedHeader,
+                    tdComp: Clientes,
+                },
+                {
+                    title: "Productos",
+                    thComp: TranslatedHeader,
+                    tdComp: Productos,
+                },
+                {
+                    title: "Descripcion",
+                    field: "descripcion",
+                    thComp: TranslatedHeader,
+                    sortable: true,
+                },
+                {
+                    title: "global.actions",
+                    thComp: TranslatedHeader,
+                    tdComp: DatatableActions,
+                    visible: true,
+                    thClass: "text-right",
+                    tdClass: "text-right td-actions",
+                    colStyle: "width: 150px;",
+                },
+            ],
+            query: { sort: "id", order: "desc", limit: 100, s: "" },
+            xprops: {
+                module: "CotizacionesIndex",
+                route: "cotizacion",
+                permission_prefix: "income_",
+            },
+        };
+    },
+    beforeDestroy() {
+        this.resetState();
+    },
+    computed: {
+        ...mapGetters("CotizacionesIndex", ["data", "total", "loading"]),
+    },
+    watch: {
+        query: {
+            handler(query) {
+                this.setQuery(query);
+                this.fetchIndexData();
+            },
+            deep: true,
         },
-        {
-        title: 'Clientes',
-        thComp: TranslatedHeader,
-        tdComp: Clientes
-        },
-        {
-        title: 'Productos',
-        thComp: TranslatedHeader,
-        tdComp: Productos
-        },
-        {
-          title: 'Descripcion',
-          field: 'descripcion',
-          thComp: TranslatedHeader,
-        sortable: true
-        },
-        {
-          title: 'global.actions',
-          thComp: TranslatedHeader,
-          tdComp: DatatableActions,
-          visible: true,
-          thClass: 'text-right',
-          tdClass: 'text-right td-actions',
-          colStyle: 'width: 150px;'
-        }
-      ],
-      query: { sort: 'id', order: 'desc', limit: 100, s: '' },
-      xprops: {
-        module: 'CotizacionesIndex',
-        route: 'cotizacion',
-        permission_prefix: 'income_'
-      }
-    }
-  },
-  beforeDestroy() {
-    this.resetState()
-  },
-  computed: {
-    ...mapGetters('CotizacionesIndex', ['data', 'total', 'loading'])
-  },
-  watch: {
-    query: {
-      handler(query) {
-        this.setQuery(query)
-        this.fetchIndexData()
-      },
-      deep: true
-    }
-  },
-  methods: {
-    ...mapActions('CotizacionesIndex', ['fetchIndexData', 'setQuery', 'resetState'])
-  }
-}
+    },
+    methods: {
+        ...mapActions("CotizacionesIndex", [
+            "fetchIndexData",
+            "setQuery",
+            "resetState",
+        ]),
+    },
+};
 </script>
